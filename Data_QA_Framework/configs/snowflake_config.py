7c9544a0307a3aac62caf50853d412772611fa1f
Tushar Sethi <61621224+tushar385@users.noreply.github.com>
2025-07-10 12:19:15 +0530

    Update snowflake_config.py

--- Data_QA_Framework/configs/snowflake_config.py
+++ Data_QA_Framework/configs/snowflake_config.py
@@ -2,11 +2,11 @@
 # SNOWFLAKE CONNECTION
 # -------------------------
 snowflake_credentials = {
-    "account": "ml69805.us-east-1",
-    "user": "tsethi",
-    "password": "Tsethi@7654321",  
-    "warehouse": "AI_MEDIUM",
-    "database": "recommendation_data_dev",
-    "schema": "ttyd_poc"
+    "account": "",
+    "user": "",
+    "password": "",  
+    "warehouse": "",
+    "database": "",
+    "schema": ""
 }
 
